6ef89ab9c73e7ffe80a451b1a6e5605693ca1d48
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-10-29 14:27:03 +0200

    Create new portfolio app

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello from Home page!</h1>')
+})
+
+
+app.get('/about', function (req, res) {
+    res.send('<h1>Hello from the About page!</h1>')
+  })
+
+app.listen(3000, () => {
+    console.log("The server is running on port 3000");
+    
+})
