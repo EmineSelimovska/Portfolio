ffae523da4a6edff50b01fde813d40b46f466c75
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-11-05 14:11:44 +0200

    Add new style in home page

--- src/app.js
+++ src/app.js
@@ -8,16 +8,37 @@ app.use(express.static(publicDirectory));
 
 app.set('view engine', 'hbs');
 
-app.get('/', function (req, res) {
+app.get("/",function(req,res){
+  res.render('home')
+})
+
+app.get('/skills', function (req, res) {
   res.render('index',{
     title: 'Home',
     name: 'Emine'
   })
 })
 
+app.get('/education', function (req, res) {
+  res.send('<h1>Hello from the Education page!</h1>')
+})
+
+app.get('/projects', function (req, res) {
+  res.send('<h1>Hello from the Projects page!</h1>')
+})
+
+
+app.get('/certification', function (req, res) {
+  res.send('<h1>Hello from the Certification page!</h1>')
+})
+
 
 app.get('/about', function (req, res) {
     res.send('<h1>Hello from the About page!</h1>')
+  })
+
+  app.get('/contact', function (req, res) {
+    res.send('<h1>Hello from the Contact page!</h1>')
   })
 
 app.listen(3000, () => {
