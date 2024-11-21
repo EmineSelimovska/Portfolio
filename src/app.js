71722b4ca4e995e4fe8bc8c43c2279a75e4c6332
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-11-21 14:03:53 +0200

    Correction in skills page

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.get('/projects', function (req, res) {
 
 
 app.get('/certification', function (req, res) {
-  res.send('<h1>Hello from the Certification page!</h1>')
+  res.render('certification')
 })
 
 
